902fff2c28c8b9811e91780471b871feb8247114
unknown <Hokkanenjjs3@gmail.com>
2021-03-11 22:57:25 +0200

    Create 2021-03-11.js

---
+++ src/js/data/2021-03-11.js
@@ -0,0 +1,32 @@
+dataSetVersion = "2021-03-11"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Generation	",
+    key: "generation",
+    tooltip: "Check this to restrict to certain Hololive generations.",
+    checked: false,
+    sub: [
+      { name: "Hololive 3rd Generation", key: "3rd" }
+    ]
+  },
+  {
+    name: "Remove Holostars",
+    key: "holostar",
+    tooltip: "Check this to remove Holostars.",
+    checked: false,
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+{
+  name: "Usada Pekora",
+  img: "II5kpjJ.png",
+  opts: {
+    generation: ["3rd"],
+  }
+},
+
+  }
+];
